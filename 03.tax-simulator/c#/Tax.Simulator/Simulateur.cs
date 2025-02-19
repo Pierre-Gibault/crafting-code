1d81bb28e622a31af6ba362fd3f78515f71b4c21
Pierre Gibault <pierre.gibault@iut-dijon.u-bourgogne.fr>
2025-02-19 10:57:44 +0100

    feat : modification de l'implémentation de CalculerImpotsAnnuel pour prendre un SituationFoyer en se basant sur les tests

--- 03.tax-simulator/c#/Tax.Simulator/Simulateur.cs
+++ 03.tax-simulator/c#/Tax.Simulator/Simulateur.cs
@@ -6,59 +6,56 @@ public static class Simulateur
     private static readonly decimal[] TauxImposition = {0.0m, 0.11m, 0.30m, 0.41m, 0.45m, 0.48m}; // Taux correspondants
 
     public static decimal CalculerImpotsAnnuel(
-        Statuts situationFamiliale,
-        decimal salaireMensuel,
-        decimal salaireMensuelConjoint,
-        int nombreEnfants)
+        SituationFoyer situationFoyer)
     {
-        if (situationFamiliale != Statuts.Celibataire && situationFamiliale != Statuts.Marie_Pacse)
+        if (situationFoyer.SituationFamiliale != Statuts.Celibataire && situationFoyer.SituationFamiliale != Statuts.Marie_Pacse)
         {
             throw new ArgumentException("Situation familiale invalide.");
         }
 
-        if (salaireMensuel <= 0)
+        if (situationFoyer.SalaireMensuel <= 0)
         {
             throw new ArgumentException("Les salaires doivent être positifs.");
         }
 
-        if (situationFamiliale == Statuts.Marie_Pacse && salaireMensuelConjoint < 0)
+        if (situationFoyer.SituationFamiliale == Statuts.Marie_Pacse && situationFoyer.SalaireMensuelConjoint < 0)
         {
             throw new InvalidDataException("Les salaires doivent être positifs.");
         }
 
-        if (nombreEnfants < 0)
+        if (situationFoyer.NbEnfants < 0)
         {
             throw new ArgumentException("Le nombre d'enfants ne peut pas être négatif.");
         }
 
         decimal revenuAnnuel;
-        if (situationFamiliale == Statuts.Marie_Pacse)
+        if (situationFoyer.SituationFamiliale == Statuts.Marie_Pacse)
         {
-            revenuAnnuel = (salaireMensuel + salaireMensuelConjoint) * 12;
+            revenuAnnuel = (situationFoyer.SalaireMensuel + situationFoyer.SalaireMensuelConjoint) * 12;
         }
         else
         {
-            revenuAnnuel = salaireMensuel * 12;
+            revenuAnnuel = situationFoyer.SalaireMensuel * 12;
         }
 
-        var baseQuotient = situationFamiliale == Statuts.Marie_Pacse ? 2 : 1;
+        var baseQuotient = situationFoyer.SituationFamiliale == Statuts.Marie_Pacse ? 2 : 1;
         decimal quotientEnfants = (decimal) Math.PI;
 
-        if (nombreEnfants == 0)
+        if (situationFoyer.NbEnfants == 0)
         {
             quotientEnfants = 0;
         }
-        else if (nombreEnfants == 1)
+        else if (situationFoyer.NbEnfants == 1)
         {
             quotientEnfants = 0.5m;
         }
-        else if (nombreEnfants == 2)
+        else if (situationFoyer.NbEnfants == 2)
         {
             quotientEnfants = 1.0m;
         }
         else
         {
-            quotientEnfants = 1.0m + (nombreEnfants - 2) * 0.5m;
+            quotientEnfants = 1.0m + (situationFoyer.NbEnfants - 2) * 0.5m;
         }
 
         var partsFiscales = baseQuotient + quotientEnfants;
