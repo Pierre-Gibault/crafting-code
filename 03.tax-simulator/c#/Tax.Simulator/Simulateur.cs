419d1795f80c4bd7d2d2201bb3b9308fe6a5ecb4
Martin <martin.de-lima@iut-dijon.u-bourgogne.fr>
2025-02-19 08:56:21 +0100

    Feat : Implémentation de la feature
    Implémentation de l'imposition sur haut revenus

--- 03.tax-simulator/c#/Tax.Simulator/Simulateur.cs
+++ 03.tax-simulator/c#/Tax.Simulator/Simulateur.cs
@@ -2,8 +2,8 @@ namespace Tax.Simulator;
 
 public static class Simulateur
 {
-    private static readonly decimal[] TranchesImposition = {10225m, 26070m, 74545m, 160336m}; // Plafonds des tranches
-    private static readonly decimal[] TauxImposition = {0.0m, 0.11m, 0.30m, 0.41m, 0.45m}; // Taux correspondants
+    private static readonly decimal[] TranchesImposition = {10225m, 26070m, 74545m, 160336m, 500000m}; // Plafonds des tranches
+    private static readonly decimal[] TauxImposition = {0.0m, 0.11m, 0.30m, 0.41m, 0.45m, 0.48m}; // Taux correspondants
 
     public static decimal CalculerImpotsAnnuel(
         string situationFamiliale,
