99fa60e060b6e3305623c5e1ae773d45596312be
Pierre Gibault <pierre.gibault@iut-dijon.u-bourgogne.fr>
2025-02-19 13:41:16 +0100

    feat: ajout de la vérification des paramètres et refactorisation du calcul des impôts annuels en utilisant LINQ a la place de boucle for

--- 03.tax-simulator/c#/Tax.Simulator/Simulateur.cs
+++ 03.tax-simulator/c#/Tax.Simulator/Simulateur.cs
@@ -8,6 +8,47 @@ public static class Simulateur
     public static decimal CalculerImpotsAnnuel(
         SituationFoyer situationFoyer)
     {
+        VerificationParametre(situationFoyer);
+
+        decimal revenuAnnuel = RevenuAnnuel(situationFoyer);
+
+        var baseQuotient = situationFoyer.SituationFamiliale == Statuts.Marie_Pacse ? 2 : 1;
+        decimal quotientEnfants = 0;
+
+        quotientEnfants = CalculerQuotientEnfant(situationFoyer.NbEnfants);
+        
+
+        decimal partsFiscales = PartsFiscales(baseQuotient, quotientEnfants, revenuAnnuel, out decimal impotParPart);
+
+        return Math.Round(impotParPart * partsFiscales, 2);
+    }
+    private static decimal PartsFiscales(int baseQuotient, decimal quotientEnfants, decimal revenuAnnuel, out decimal impotParPart)
+    {
+        var partsFiscales = baseQuotient + quotientEnfants;
+        var revenuImposableParPart = revenuAnnuel / partsFiscales;
+        
+        impotParPart = TranchesImposition
+            .Select((tranche, index) => new
+            {
+                LowerBound = index > 0 ? TranchesImposition[index - 1] : 0,
+                UpperBound = tranche,
+                Rate = TauxImposition[index]
+            })
+            .Where(t => revenuImposableParPart > t.LowerBound)
+            .Sum(t => (Math.Min(revenuImposableParPart, t.UpperBound) - t.LowerBound) * t.Rate);
+        
+        if (revenuImposableParPart > TranchesImposition[^1])
+        {
+            impotParPart += (revenuImposableParPart - TranchesImposition[^1]) * TauxImposition[^1];
+        }
+
+        return partsFiscales;
+    }
+
+
+    private static void VerificationParametre(SituationFoyer situationFoyer)
+    {
+
         if (situationFoyer.SituationFamiliale != Statuts.Celibataire && situationFoyer.SituationFamiliale != Statuts.Marie_Pacse)
         {
             throw new ArgumentException("Situation familiale invalide.");
@@ -22,40 +63,6 @@ public static class Simulateur
         {
             throw new InvalidDataException("Les salaires doivent être positifs.");
         }
-
-        decimal revenuAnnuel = RevenuAnnuel(situationFoyer);
-
-        var baseQuotient = situationFoyer.SituationFamiliale == Statuts.Marie_Pacse ? 2 : 1;
-        decimal quotientEnfants = 0;
-
-        quotientEnfants = CalculerQuotientEnfant(situationFoyer.NbEnfants);
-        
-
-        var partsFiscales = baseQuotient + quotientEnfants;
-        var revenuImposableParPart = revenuAnnuel / partsFiscales;
-
-        decimal impot = 0;
-        for (var i = 0; i < TranchesImposition.Length; i++)
-        {
-            if (revenuImposableParPart <= TranchesImposition[i])
-            {
-                impot += (revenuImposableParPart - (i > 0 ? TranchesImposition[i - 1] : 0)) * TauxImposition[i];
-                break;
-            }
-            else
-            {
-                impot += (TranchesImposition[i] - (i > 0 ? TranchesImposition[i - 1] : 0)) * TauxImposition[i];
-            }
-        }
-
-        if (revenuImposableParPart > TranchesImposition[^1])
-        {
-            impot += (revenuImposableParPart - TranchesImposition[^1]) * TauxImposition[^1];
-        }
-
-        var impotParPart = impot;
-
-        return Math.Round(impotParPart * partsFiscales, 2);
     }
     private static decimal RevenuAnnuel(SituationFoyer situationFoyer)
     {
