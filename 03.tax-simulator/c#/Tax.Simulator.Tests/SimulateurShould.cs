37b3f847645c610119f86899b73ce62759b0ef38
Pierre Gibault <pierre.gibault@iut-dijon.u-bourgogne.fr>
2025-02-19 09:11:45 +0100

    feat: Ajout de test pour la User Story 1: Calcul de l'impôt pour un célibataire

--- 03.tax-simulator/c#/Tax.Simulator.Tests/SimulateurShould.cs
+++ 03.tax-simulator/c#/Tax.Simulator.Tests/SimulateurShould.cs
@@ -30,7 +30,7 @@ public class SimulateurShould
         
         impots.Should().Be(223508.56m);
     }
-    
+
     [Fact(DisplayName = "Marié/Pacsé, 25 000€/mois, 30 000€/mois, 2 enfants -> 234 925.68€")]
     public void TestImpotsSurRevenusConjoints()
     {
@@ -38,9 +38,37 @@ public class SimulateurShould
         var salaireMensuel = 25000m;
         var salaireMensuelConjoint = 30000m;
         var nombreEnfants = 2;
-        
+
         var impots = Simulateur.CalculerImpotsAnnuel(situationFamiliale, salaireMensuel, salaireMensuelConjoint, nombreEnfants);
-        
+
         impots.Should().Be(234925.68m);
     }
+    [Fact]
+    public void TestCalculImpotsPourUnCelibataire()
+    {
+        var situationFamiliale = "Célibataire";
+        var salaireMensuel = 2000m;
+        var salaireMensuelConjoint = 0m;
+        var nombreEnfants = 0;
+        var expectedResult = 1515.25m;
+        
+        var result = Simulateur.CalculerImpotsAnnuel(situationFamiliale, salaireMensuel, salaireMensuelConjoint, nombreEnfants);
+        
+        result.Should().Be(expectedResult);
+    }
+    
+    [Fact]
+    public void TestLanceraUneExceptionPourUnSalaireNégatif()
+    {
+        var situationFamiliale = "Célibataire";
+        var salaireMensuel = -2000m;
+        var salaireMensuelConjoint = 0m;
+        var nombreEnfants = 0;
+        
+        Action act = () => Simulateur.CalculerImpotsAnnuel(situationFamiliale, salaireMensuel, salaireMensuelConjoint, nombreEnfants);
+        
+        act.Should().Throw<ArgumentException>().WithMessage("Les salaires doivent être positifs.");
+    }
+    
+    
 }
